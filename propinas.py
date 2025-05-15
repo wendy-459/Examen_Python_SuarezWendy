9565812a3199be1e03f25ca19827d774eee95f0a
wendy-459 <suarezherrerawendyvalentina@gmail.com>
2025-05-15 18:11:18 -0500

    Update propinas.py

--- propinas.py
+++ propinas.py
@@ -1,10 +1,10 @@
 def calcular_propina(total, porcentaje):
-    return round(total * (porcentaje / 100), 2)
+    return total * (porcentaje / 100)
 
-def calcular_total_con_propina(total, propina):
-    return round(total + propina, 2)
+def calcular_total_propina(total, propina):
+    return total + propina
 
 def dividir_total(total, personas):
     if personas <= 0:
         return None
-    return round(total / personas, 2)
+    return total / personas
